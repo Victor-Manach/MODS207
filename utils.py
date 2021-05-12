a2c5aa72adc6f71c144d2c160e6ff6a66d8ee56e
Victor Manach <victor.manach@telecom-paris.fr>
2021-05-12 16:49:24 +0200

    Add a score to the profile and analyse the skills and expertise for the most active profiles

--- utils.py
+++ utils.py
@@ -70,7 +70,6 @@ def profile_info(url: str) -> dict:
 	"""
 	Collect all information available on the webpage of the freelancer
 	"""
-	reviews = []
 	resp = requests.get(url)
 	while resp.status_code!=200:
 		time.sleep(60)
@@ -79,7 +78,6 @@ def profile_info(url: str) -> dict:
 	info = profile.xpath('.//div[@class="container member-info-container full-width"]')[0]
 	projects = info.xpath('.//div[@class="memStats-container "]')[0]
 	main_info = info.xpath('.//div[@class="sidebar-box clearfix"]')[0]
-	reviews_container = info.xpath('.//div[@class="project-list-feedback"]')
 	name = main_info.xpath('.//div[@class="details "]')[0].xpath('.//h1')[0].text_content().strip()
 	location = main_info.xpath('.//div[@class="member-location"]')[0].text_content().strip()
 	languages = main_info.xpath('.//div[@class="member-languages clearfix"]')[0].text_content().strip()
@@ -110,14 +108,8 @@ def profile_info(url: str) -> dict:
 	nb_buyers = int(projects.xpath('.//div[@class="memberStats-item u-mgb--1"]')[0].xpath('.//div[@class="insights-value"]')[0].text_content().strip())
 	last_active = projects.xpath('.//div[@class="memberStats-item item-margin"]')[0].xpath('.//div[@class="insights-value"]')[0].text_content().strip()
 	
-	if len(reviews_container) > 0:
-		for full_review in reviews_container:
-			if len(full_review.xpath('.//div[@class="col-xs-10 right-col"]')) > 0:
-				review = full_review.xpath('.//div[@class="col-xs-10 right-col"]')[0].xpath('.//p')[0].text_content()
-				reviews.append(review)
-		gender = guess_gender_from_reviews(reviews)
-	else:
-		gender = None
+	gender = get_gender(profile_ul)
+	
 	info = {
 		'name': name,
 		'location': location,
@@ -146,79 +138,31 @@ def get_nb_projects_pages(profile_url: str) -> int:
 	
 	return int(nb_projects_pages)
 
-def get_projects_id(profile_url: str, projects_page: int) -> list:
-	id_list = []
-	profile_url += '?Projects_page={}'.format(projects_page)
-	resp = requests.get(profile_url)
-	while resp.status_code != 200:
-		time.sleep(60)
-		resp = requests.get(profile_url)
-	
-	projects = html.fromstring(resp.text)
-	projects_ids = projects.xpath('.//div[@class="keys"]')[0].xpath('.//span')
-	for project_id in projects_ids:
-		id_list.append(int(project_id.text_content()))
-	
-	return id_list
-
-
-def get_all_reviews(profile_url: str) -> str:
-	nb_projects_pages = get_nb_projects_pages(profile_url)
-	project_ids = {}
-	for i in range(1,nb_projects_pages+1):
-		project_ids['{}'.format(i)] = get_projects_id(profile_url, i)
-		if i%10==0:
-			print(str(i)+' pages checked for the project ids')
-	print(project_ids)
-	reviews_dict, reviews_list = {}, []
-	cookies = {
-    	'_ga_7KBXBWQ5QT': 'GS1.1.1620422045.2.1.1620423884.60',
-    	'_ga': 'GA1.2.1664462320.1620077339',
-    	'_gid': 'GA1.2.1255311243.1620422047',
-    	'_uetsid': '28484f00af7911ebbd2dd9d9b74b9c95',
-    	'_uetvid': '25d97cc09e2b11ebbce54387f8524914',
-    	'PHPSESSID': '121a79acac4a174e42d4a61cf4126be3',
-    	'YII_CSRF_TOKEN': 'bXFvY1dKbWtCejFPTkRlN210SmZFN2ZTbEk2R3h4NTfPzFVHVguGmY5RzU-YDV7fa2yXqfaJ4qtkrO4ir9zs4A',
-    	'connect.sid': 's%3AugbmU4a_aaetn78tHOhXoDenzLY0Y4aP.a%2F1C6iaa6bFOGzD%2BwdQkUwSJXKua%2BQt%2BnSqh%2FzJ5hjQ',
-    	'_gcl_au': '1.1.89486104.1620077339',
-    	'api': '6bb881afc885f120138a6f7b73d85d90c80be4b5YTo0OntpOjA7aTo1NzYwODc3O2k6MTtzOjU6IjBiOGZmIjtpOjI7aTo4NjQwMDAwO2k6MzthOjA6e319',
-    	'mid': '1618519355022490900792955',
-	}
-	headers = {
-    	'Accept': '*/*',
-    	'Connection': 'keep-alive',
-    	'Accept-Language': 'en-us',
-    	'Host': 'www.peopleperhour.com',
-    	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
-    	'Referer': profile_url,
-    	'Accept-Encoding': 'gzip, deflate, br',
-    	'X-Requested-With': 'XMLHttpRequest',
-    	'sentry-trace': '0a33dbde2a7548258e8a74c8069e280b-99131d7007beb685-0',
-	}
-	for page_nb,project_id in project_ids.items():
-		for ids in project_id:
-			params = (
-				('Projects_page', page_nb),
-				('ajax', 'activity-list'),
-				('fPageH{}'.format(ids), str(1000)),
-			)
-			resp = requests.get(profile_url, headers=headers, params=params, cookies=cookies)
-			while resp.status_code != 200:
-				time.sleep(60)
-				resp = requests.get(profile_url, headers=headers, params=params, cookies=cookies)
-			reviews = html.fromstring(resp.text)
-			hidden_reviews = reviews.xpath('.//p')
-			for review in hidden_reviews:
-				reviews_list.append(review.text_content())
-			reviews_dict['{}'.format(ids)] = reviews_list
-		if int(page_nb)%10==0:
-			print(str(page_nb)+' pages scrape for the reviews over '+str(nb_projects_pages))
-	print(len(reviews_dict))
-	print(reviews_dict)
-
-get_all_reviews('https://www.peopleperhour.com/freelancer/design/maria-experienced-team-of-graphic-wjawjq')
-
-
+def get_gender(profile_url: str) -> str:
+	reviews = []
+	nb_pages = get_nb_projects_pages(profile_url)
+	for page in range(1,nb_pages+1):
+		print(profile_url+'?Projects_page={}'.format(page))
+		resp = requests.get(profile_url+'?Projects_page={}'.format(page))
+		while resp.status_code != 200:
+			time.sleep(60)
+			resp = requests.get(profile_url)
+		profile = html.fromstring(resp.text)
+		info = profile.xpath('.//div[@class="container member-info-container full-width"]')[0]
+		reviews_container = info.xpath('.//div[@class="project-list-feedback"]')
+		if len(reviews_container) > 0:
+			for full_review in reviews_container:
+				if len(full_review.xpath('.//div[@class="col-xs-10 right-col"]')) > 0:
+					review = full_review.xpath('.//div[@class="col-xs-10 right-col"]')[0].xpath('.//p')[0].text_content()
+					reviews.append(review) 
+	if len(reviews) > 0:
+		gender = guess_gender_from_reviews(reviews)
+	else:
+		gender = None
+	return gender
+		
+get_gender('https://www.peopleperhour.com/freelancer/business/teodora-popescu-certified-translator-digital-zamnnqz')
+		
 def get_exchange_rates(currencies: list, date: str) -> dict:
 	"""
 	Determine the exchange rate between the base currency and USD on the specified date
